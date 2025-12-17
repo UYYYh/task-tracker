57f989e24a280a2381b4deac8cdd2364d4aa07c4
Kevin Wang <113120341+UYYYh@users.noreply.github.com>
2025-12-17 02:28:29 +0000

    FEAT: implement DbUserTaskManager.createTask()

--- src/main/kotlin/task/tracker/infra/db/DbUserTaskManager.kt
+++ src/main/kotlin/task/tracker/infra/db/DbUserTaskManager.kt
@@ -4,11 +4,74 @@ import com.example.task.tracker.dto.TaskDTO
 import kotlinx.datetime.Instant
 import task.tracker.app.TaskID
 import task.tracker.app.TaskSpec
+import task.tracker.app.UserID
 import task.tracker.app.UserTaskManager
+import java.sql.Connection
+import java.sql.ResultSet
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.util.UUID
+import javax.sql.DataSource
 
-class DbUserTaskManager : UserTaskManager {
+private fun ResultSet.toTaskDTO(): TaskDTO =
+    TaskDTO(
+        id = getObject("id", UUID::class.java).toString(),
+        title = getString("title"),
+        description = getString("description"),
+        creationTime =
+            getObject(
+                "created_at",
+                OffsetDateTime::class.java,
+            ).toInstant().let { Instant.fromEpochMilliseconds(it.toEpochMilli()) },
+        deadline =
+            getObject(
+                "deadline",
+                OffsetDateTime::class.java,
+            )?.toInstant()?.let { Instant.fromEpochMilliseconds(it.toEpochMilli()) },
+        completionTime =
+            getObject(
+                "completed_at",
+                OffsetDateTime::class.java,
+            )?.toInstant()?.let { Instant.fromEpochMilliseconds(it.toEpochMilli()) },
+    )
+
+private fun Instant.toOffsetDateTime(): OffsetDateTime =
+    OffsetDateTime.ofInstant(
+        java.time.Instant.ofEpochMilli(this.toEpochMilliseconds()),
+        ZoneOffset.UTC,
+    )
+
+private fun OffsetDateTime.toKxInstant(): Instant = Instant.fromEpochMilliseconds(this.toInstant().toEpochMilli())
+
+class DbUserTaskManager(
+    private val userID: UserID,
+    private val dataSource: DataSource,
+) : UserTaskManager {
     override fun createTask(spec: TaskSpec): TaskDTO {
-        TODO("Not yet implemented")
+        val sql =
+            """
+            INSERT INTO tasks (user_id, title, description, deadline)
+            VALUES (?, ?, ?, ?)
+            RETURNING id, title, description, created_at, deadline, completed_at
+            """.trimIndent()
+
+        return dataSource.withConnection { c ->
+            c.prep(
+                sql,
+                bind = {
+                    setObject(1, userID.raw)
+                    setString(2, spec.title)
+                    setString(3, spec.description)
+                    setObject(4, spec.deadline?.toOffsetDateTime())
+                },
+                read = {
+                    executeQuery().use { rs ->
+                        rs.next()
+                        rs.toTaskDTO()
+                    }
+                },
+            )
+        }
     }
 
     override fun deleteTask(taskID: TaskID): Boolean {
